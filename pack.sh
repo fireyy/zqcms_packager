387c037b1d1f90eb81eacb1596ef0498f1dd7968
fireyy <fireyy@gmail.com>
2013-01-10 11:30:13 +0800

    更新版本的时候刷新对应文件的cdn缓存

---
+++ pack.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+cd zqcms
+git pull -f
+cd ..
+python pack.py
+echo "=== sync files to cdn"
+./qrsync zqcms_update.json
+echo "=== files sync done"
+#TODO 使用qboxrsctl清理cdn的缓存
+source config
+./qboxrsctl login $USERNAME $PADDWORD
+echo "=== refresh cdn caches"
+./qboxrsctl refresh update
+./qboxrsctl cdn/refresh $CDN_PATH/verinfo.txt
+./qboxrsctl cdn/refresh $CDN_PATH/latest.zip
+ZQCMS_VERSIONS=$(cat dist/source/caches/update/ver.txt)
+if [ -n "$ZQCMS_VERSIONS" ]; then
+    cat "dist/$ZQCMS_VERSIONS.file.txt" | while read line
+    do
+        ./qboxrsctl cdn/refresh $CDN_PATH/$line
+    done
+else
+    echo "!!!=== ZQCMS_VERSIONS is null"
+fi
+echo "=== All done!"
